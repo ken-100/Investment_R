5a020b2bdfa202c1d1153318358cf73280c5e47c
Ken.Y <69562939+ken-100@users.noreply.github.com>
2022-03-29 01:39:18 +0900

    Create BDH_FX.R

---
+++ BBG/BDH_FX.R
@@ -0,0 +1,39 @@
+install.packages(c("Rblpapi"))
+
+library(Rblpapi)
+
+blpConnect()
+from = "20200101"
+to = "20220228"
+
+L = c("USD","EUR","JPY","GBP")
+T1 = {}
+T2 = {}
+
+for (i in 1:length(L))
+{
+  for ( j in 1:length(L))
+  {
+    if ( i > j)
+    {
+      tmp = paste( L[i],L[j], sep="")
+      T1 = append(T1, tmp)
+      T2 = append(T2, paste( tmp," CMPN Curncy", sep=""))
+    }
+  }
+}
+
+
+for (i in 1:length(T1))
+{ tmp = bdh(T2[i],
+            "CHG_PCT_1D",
+            start = from,
+            end = to,
+            options = c("periodicitySelection" = "WEEKLY"))
+  if (i == 1) {df = tmp}
+  else {df = cbind(df,tmp[,2])}
+}
+
+colnames(df)= c("Date",T1)
+print(dim(df))
+df
